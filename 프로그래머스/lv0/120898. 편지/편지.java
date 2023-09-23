1d9dd861e8ef663607809ebfe578aca1e94f7cbe
minahYu <96765898+minahYu@users.noreply.github.com>
2023-09-23 22:08:41 +0900

    [level 0] Title: 편지, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120898. 편지/편지.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        int msg_len = message.length();
+        
+        answer = msg_len * 2;
+        return answer;
+    }
+}
