2a22ea6912c321c04abb1139a1f925a2ffba558b
minahYu <96765898+minahYu@users.noreply.github.com>
2023-12-12 09:20:49 +0900

    [level 1] Title: 폰켓몬, Time: 5.52 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : nums) {
+            set.add(num);
+        }
+        
+        answer = set.size() < nums.length/2 ? set.size() : nums.length/2;
+        
+        return answer;
+    }
+}
