045d9fbd7230929e44a2be292e628311f6bb4862
minahYu <96765898+minahYu@users.noreply.github.com>
2024-02-23 21:35:19 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,16 +1,20 @@
 class Solution {
     public int solution(int num) {
+        long calc = num;
         int answer = 0;
-        long lNum = (long)num;
         
-        while(lNum != 1) {
-            if(answer == 500) {
+        while(calc > 1) {
+            if(answer >= 500) {
                 answer = -1;
                 break;
             }
-        
-            lNum = (lNum%2 == 0) ? (lNum/2) : (lNum*3 + 1);
             answer++;
+            if(calc%2 == 0) {
+                calc /= 2;
+            } else {
+                calc *= 3;
+                calc++;
+            } 
         }
         
         return answer;
