dbe6f025a9bff87e92965eff2ec8f3fd2903a561
minahYu <96765898+minahYu@users.noreply.github.com>
2024-03-08 18:17:20 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 7.77 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 최대공약수와 최소공배수 - 12940 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12940) 
 
 ### 성능 요약
 
-메모리: 81.5 MB, 시간: 0.13 ms
+메모리: 82.6 MB, 시간: 7.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 06일 23:03:58
+2024년 03월 08일 18:17:19
 
 ### 문제 설명
 
--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -3,18 +3,27 @@ import java.lang.Math;
 class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
+        int temp = 0;
+        int copyN = n, copyM = m;
         
         if(m%n == 0) {
             answer[0] = n;
             answer[1] = m;
         } else {
-            for(int i=(int)Math.sqrt(m*n); i>0; i--) {
-                if(n%i == 0 && m%i == 0) {
-                    answer[0] = i;
-                    break;
-                }
+            while(m%n != 0) {
+                temp = m%n;
+                m = n;
+                n = temp;
+                System.out.println(n + " " + m);
             }
-            answer[1] = (n/answer[0]) * (m/answer[0]) * answer[0];
+            // for(int i=(int)Math.sqrt(m*n); i>0; i--) {
+            //     if(n%i == 0 && m%i == 0) {
+            //         answer[0] = i;
+            //         break;
+            //     }
+            // }
+            answer[0] = n;
+            answer[1] = (copyN/answer[0]) * (copyM/answer[0]) * answer[0];
         }
         return answer;
     }
