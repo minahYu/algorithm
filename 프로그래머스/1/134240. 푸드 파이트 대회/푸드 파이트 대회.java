a7ff26b151717d7c5615ea834cff8b736168565b
minahYu <96765898+minahYu@users.noreply.github.com>
2024-03-16 22:39:34 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 2.69 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=1; i<food.length; i++) {
+            for(int j=0; j<food[i]/2; j++)
+                sb.append(i);
+        }
+        answer += sb.toString();
+        answer += "0";
+        answer += sb.reverse().toString();
+        
+        return answer;
+    }
+}
