a62c6c62dd9268404d8ae856ef70faf10b8abbcf
minahYu <96765898+minahYu@users.noreply.github.com>
2024-04-13 16:51:33 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.02 ms, Memory: 69.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,32 +1,27 @@
 class Solution {
-    public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
-        int cnt = 0;
-        int unknown = 0;
+    public int[] solution(int[] lottos, int[] win_nums) { 
+        int[] answer = { 1, 6 };
+        int sameCount = 0;
+        int zeroCount = 0;
         
-        for(int i=0; i<win_nums.length; i++) {
-            if (lottos[i] == 0) {
-                unknown++;
+        for(int i=0; i<lottos.length; i++) {
+            if(lottos[i] == 0) {
+                zeroCount++;
                 continue;
             }
-                    
             for(int j=0; j<win_nums.length; j++) {
                 if(lottos[i] == win_nums[j])
-                    cnt++;
+                    sameCount++;
             }
         }
         
-        if(cnt >=2 && cnt <= 6) {
-            answer[1] = 7-cnt;
-        } else {
-            answer[1] = 6;
-        }
+        answer[1] = 7-sameCount;
+        answer[0] = answer[1]-zeroCount;
         
-        if(cnt+unknown >=2 && cnt+unknown <= 6) {
-            answer[0] = 7-(cnt+unknown);
-        } else {
+        if(answer[0] > 6)
             answer[0] = 6;
-        }
+        if(answer[1] > 6)
+            answer[1] = 6;
         
         return answer;
     }
