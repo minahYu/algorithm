1681aa070c735244a3ce1e88c1845e6a20eaa18f
minahYu <96765898+minahYu@users.noreply.github.com>
2024-06-06 16:58:54 +0900

    [level 2] Title: 숫자의 표현, Time: 1.71 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        int sum = 1;
+        
+        int start_index = 1;
+        int end_index = 1;
+        
+        while(end_index != n) {
+            if(sum < n) {
+                end_index++;
+                sum += end_index;
+            } else if(sum > n) {
+                sum -= start_index;
+                start_index++;
+            } else {
+                answer++;
+                end_index++;
+                sum += end_index;
+            }
+        }
+        
+        return answer;
+    }
+}
