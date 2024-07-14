e537225ee9e47d2f456b436f00b70e17cbe82d9d
minahYu <96765898+minahYu@users.noreply.github.com>
2024-07-14 22:39:32 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(st.nextToken());
+
+		for(int i=0; i<T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			int gcd = getGcd(A, B);
+			int lcm = (A * B) / gcd;
+			sb.append(lcm).append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+	
+	private static int getGcd(int a, int b) {
+		while(b != 0) {
+			int temp = b;
+			b = a % b;
+			a = temp;
+		}
+		return a;
+	}
+}
