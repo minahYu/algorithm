2f9d451d3450e3b9c2798bc43aa4d39ddc8d1625
minahYu <96765898+minahYu@users.noreply.github.com>
2024-12-27 23:15:16 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=2; i<=n; i++) {
+            boolean prime = true;
+            
+            for(int j=2; j<=Math.sqrt(i); j++) {
+                if(i%j == 0) {
+                    prime = false;
+                    break;
+                }
+            }
+            
+            if(!prime) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
