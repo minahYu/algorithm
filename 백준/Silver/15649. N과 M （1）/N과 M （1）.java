0b3146f833c692e3f8aa2a7adf945013aa21ae11
minahYu <96765898+minahYu@users.noreply.github.com>
2025-01-28 01:48:54 +0900

    [Silver III] Title: N과 M (1), Time: 1884 ms, Memory: 78484 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	static boolean[] visited; 
+	static int[] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		arr = new int[M];
+		visited = new boolean[N];
+		DFS(N, M, 0);	
+	}
+	
+	private static void DFS(int N, int M, int depth) {
+		if(depth == M) {  
+			for(int i : arr) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for(int i=0; i<N; i++) { 
+			if(!visited[i]) {
+				visited[i] = true;
+				arr[depth] = i+1;
+				DFS(N, M, depth+1);
+				visited[i] = false;
+			}
+		}
+	}
+}
