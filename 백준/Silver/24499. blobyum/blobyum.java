60a23e0159da71c74ba9b282a249a0d9c93bdd35
minahYu <96765898+minahYu@users.noreply.github.com>
2025-02-04 22:06:14 +0900

    [Silver IV] Title: blobyum, Time: 248 ms, Memory: 24148 KB -BaekjoonHub

--- 백준/Silver/24499. blobyum/blobyum.java
+++ 백준/Silver/24499. blobyum/blobyum.java
@@ -11,12 +11,11 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N];
 		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
 		int sum = 0;
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
-			
 			if(i<K) {
 				sum += arr[i];
 			}
