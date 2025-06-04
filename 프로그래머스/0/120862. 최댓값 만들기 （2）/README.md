5cc54690c75a48bae7bc929e4dab0d7d73662692
minahYu <96765898+minahYu@users.noreply.github.com>
2025-06-04 17:07:14 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.68 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 최댓값 만들기 (2) - 120862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120862) 
 
 ### 성능 요약
 
-메모리: 72 MB, 시간: 0.51 ms
+메모리: 88 MB, 시간: 0.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 05일 19:08:25
+2025년 06월 04일 17:07:12
 
 ### 문제 설명
 
--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,34 +1,13 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
+        int answer = Integer.MIN_VALUE;
         
-        if(numbers.length == 2) {
-            return numbers[0] * numbers[1];
-        }
-        
-        List<Integer> positiveNums = new ArrayList<>();
-        List<Integer> negativeNums = new ArrayList<>();
         for(int i=0; i<numbers.length; i++) {
-            if(numbers[i] > 0) {
-                positiveNums.add(numbers[i]);
-            } else if(numbers[i] < 0) {
-                negativeNums.add(numbers[i]);
+            for(int j=0; j<numbers.length; j++) {
+                if(i != j) {
+                    answer = Math.max(answer, numbers[i] * numbers[j]);
+                }
             }
-        }
-        
-        Collections.sort(positiveNums, Collections.reverseOrder());
-        Collections.sort(negativeNums, Collections.reverseOrder());
-        
-        if(negativeNums.size() >= 2 && positiveNums.size() >= 2) {
-            answer = Math.max(positiveNums.get(0) * positiveNums.get(1), negativeNums.get(0) * negativeNums.get(1));
-        } else if(positiveNums.size() >= 2) {
-            answer = positiveNums.get(0) * positiveNums.get(1);
-        } else if(negativeNums.size() >= 2) {
-            answer = negativeNums.get(0) * negativeNums.get(1);
         }
         
         return answer;
